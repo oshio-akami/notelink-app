6112fd04ad9cb79dec353f59923c7b3e8cd0d0c1
shion_inoue <shion.akasal@gmail.com>
2025-03-21 11:54:11 +0900

    ログイン画面を実装

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -8,17 +8,7 @@ export default function Page() {
   const { data: session, status } = useSession();
   return (
     <div>
-      {status === "authenticated" ? (
-        <div>
-          <p>セッションの期限：{session.expires}</p>
-          <p>ようこそ、{session.user?.name}さん</p>
-          <div>
-            <Logout />
-          </div>
-        </div>
-      ) : (
-        <Login />
-      )}
+      <Login />
     </div>
   );
 }
--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,19 +1,20 @@
-import { useSession,signIn } from "next-auth/react"
+import { Input ,PasswordInput,Button} from "@mantine/core"
+import styles from "./login.module.css"
+import {signIn } from "next-auth/react"
 
 export function Login(){
-  const session=useSession();
-
-  if(!session||session.data?.user==undefined){
-    return(
-      <div>
-        <p>ログインしていません</p>
-        <button onClick={()=>signIn("google",{},{prompt:"login"}) }>
-          Googleでログインする
-        </button>
-      </div>
-    )
-  }
   return(
-    <p>ログイン画面</p>
+    <div className={styles.loginPage}>
+      <h1>Login</h1>
+      <div className={styles.loginWindow}>
+        <ul>
+          <li><Input placeholder="e-mail"></Input></li>
+        <li><PasswordInput placeholder="password"></PasswordInput></li>
+        <li><Button>Login</Button></li>
+        <li><p className={styles.split}>or</p></li>
+        <li><img src="web_light_sq_SI.svg" alt="" onClick={()=>signIn("google",{ callbackUrl:"/dashboard" })}/></li>
+        </ul>
+      </div>
+    </div>
   )
 }
