03e1c5d8e18cc4ae0dfc3bfa1aecbc180572daa5
shion_inoue <shion.akasal@gmail.com>
2025-03-27 18:09:14 +0900

    マイグレーションファイル追加

---
+++ drizzle/0002_curly_bruce_banner.sql
@@ -0,0 +1,32 @@
+CREATE TABLE "account" (
+	"userId" text NOT NULL,
+	"type" text NOT NULL,
+	"provider" text NOT NULL,
+	"providerAccountId" text NOT NULL,
+	"refresh_token" text,
+	"access_token" text,
+	"expires_at" integer,
+	"token_type" text,
+	"scope" text,
+	"id_token" text,
+	"session_state" text
+);
+--> statement-breakpoint
+CREATE TABLE "authenticator" (
+	"credentialID" text NOT NULL,
+	"userId" text NOT NULL,
+	"providerAccountId" text NOT NULL,
+	"credentialPublicKey" text NOT NULL,
+	"counter" integer NOT NULL,
+	"credentialDeviceType" text NOT NULL,
+	"credentialBackedUp" boolean NOT NULL,
+	"transports" text,
+	CONSTRAINT "authenticator_credentialID_unique" UNIQUE("credentialID")
+);
+--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "name" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "email" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "image" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "emailVerified" timestamp;--> statement-breakpoint
+ALTER TABLE "account" ADD CONSTRAINT "account_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "authenticator" ADD CONSTRAINT "authenticator_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
