67477d55afd1c3199eaf7f8f98f7c42d3883cb29
shion_inoue <shion.akasal@gmail.com>
2025-03-27 19:40:54 +0900

    データベースのテスト用apiコメントアウト

--- src/app/api/[...route]/posts.ts
+++ src/app/api/[...route]/posts.ts
@@ -1,16 +1,15 @@
 
 import { Hono } from "hono";
-import {usersTable}from '@/db/schema';
-import {db} from "@/db/index";
+//import {db} from "@/db/index";
 
 const posts=new Hono()
 .get("/", async (c) => {
   return c.json({ name: "Root!" });
-})
-.get("/table", async (c) => {
+});
+/*.get("/table", async (c) => {
   const result=await db.select().from(usersTable).execute(); 
   return c.json(result);
-});
+});*/
 
 
 export default posts
