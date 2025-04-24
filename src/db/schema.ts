fff9f6f90f9500b5710cc9330abb26c7ef28c0c6
shion_inoue <shion.akasal@gmail.com>
2025-04-24 10:43:44 +0900

    schemaファイル修正

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,14 +9,60 @@ import {
 } from "drizzle-orm/pg-core";
 import { sql } from "drizzle-orm";
 
+import type { AdapterAccountType } from "next-auth/adapters"
+
 export const users = pgTable("users", {
-  id: uuid("id").primaryKey(),
-  displayName: text("name"),
+  id: uuid("id").primaryKey().$defaultFn(() => crypto.randomUUID()),
+  name: text("name"),
+  email: text("email").unique(),
+  emailVerified: timestamp("email_verified", { mode: "date" }),
+  image: text("image"),
+})
+
+export const accounts = pgTable(
+  "account",
+  {
+    userId: uuid("user_id")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").$type<AdapterAccountType>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("provider_account_id").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+  },
+  (account) => [
+    {
+      compoundKey: primaryKey({
+        columns: [account.provider, account.providerAccountId],
+      }),
+    },
+  ]
+)
+
+export const sessions = pgTable("session", {
+  sessionToken: text("session_token").primaryKey(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  expires: timestamp("expires", { mode: "date" }).notNull(),
+})
+
+/*ユーザーのprofile情報*/
+export const userProfiles=pgTable("user_profiles",{
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }).primaryKey(),
+  displayName:text("display_name").notNull(),
   image: text("image"),
   about:text("about"),
-  currentGroupId:uuid("active_group").references(() => groups.groupId, { onDelete: "set null" }),
-});
-
+  currentGroupId:uuid("current_group_id").references(() => groups.groupId, { onDelete: "set null" }),
+})
 
 //ユーザーの役職
 export const roles = pgTable("roles", {
@@ -61,6 +107,7 @@ export const groupInvites=pgTable("group_invites",{
 
 export const schema={
   users,
+  userProfiles,
   roles,
   groups,
   groupMembers,
