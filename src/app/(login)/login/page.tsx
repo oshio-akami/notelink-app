9de837977a262fce098e34468f566f8bb5d1bd63
shion_inoue <shion.akasal@gmail.com>
2025-04-28 11:05:02 +0900

    url修正

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -1,19 +1,21 @@
 import { LoginForm } from "./_components/loginForm/LoginForm";
 import { redirect } from "next/navigation";
 import {getClient} from "@/libs/hono";
+import { auth } from "@/auth";
 
 export const runtime = "edge";
 
 export default async function Page() {
   const client=await getClient();
   const res=await client.api.user.currentGroup.$get();
-  console.log(res);
-  const raw=await res.text();
-  console.log(raw);
   const body=await res.json();
   const {currentGroup}=body;
   if(currentGroup){   
     redirect(`/group/${currentGroup}/home`);
+  }
+  const session=await auth();
+  if(session?.user?.id){
+    redirect(`/join-group`);
   }
   return (
     <div>
