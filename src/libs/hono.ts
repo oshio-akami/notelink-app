d99487f28384e7daf307573abe1fe84963f4139e
shion_inoue <shion.akasal@gmail.com>
2025-04-28 13:22:29 +0900

    デバッグ用のコンソール追加

--- src/libs/hono.ts
+++ src/libs/hono.ts
@@ -5,6 +5,8 @@ import { headers } from "next/headers";
 export const getClient=async()=>{
   const headerList=await headers();
   const cookie=headerList.get("cookie");
+  console.log("pages_url:"+process.env.CF_PAGES_URL);
+  console.log("public_url:"+process.env.NEXT_PUBLIC_URL);
   const client=hc<AppType>(process.env.CF_PAGES_URL||process.env.NEXT_PUBLIC_URL!,{
     init:{
       headers:{
