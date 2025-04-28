0fe278d3b65ae5b008f5512a8fd1e207684f4b2d
shion_inoue <shion.akasal@gmail.com>
2025-04-28 13:39:02 +0900

    デバッグ位置の変更

--- src/libs/hono.ts
+++ src/libs/hono.ts
@@ -3,10 +3,10 @@ import type { AppType } from "@/app/api/[...route]/route";
 import { headers } from "next/headers";
 
 export const getClient=async()=>{
-  const headerList=await headers();
-  const cookie=headerList.get("cookie");
   console.log("pages_url:"+process.env.CF_PAGES_URL);
   console.log("public_url:"+process.env.NEXT_PUBLIC_URL);
+  const headerList=await headers();
+  const cookie=headerList.get("cookie");
   const client=hc<AppType>(process.env.CF_PAGES_URL||process.env.NEXT_PUBLIC_URL!,{
     init:{
       headers:{
