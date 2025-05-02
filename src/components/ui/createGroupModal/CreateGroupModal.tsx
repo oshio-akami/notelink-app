f0aca2b548d0cd29a70184b3150853535c27cce4
shion_inoue <shion.akasal@gmail.com>
2025-05-02 13:08:30 +0900

    グループへの参加　仮作成

--- src/components/ui/createGroupModal/CreateGroupModal.tsx
+++ src/components/ui/createGroupModal/CreateGroupModal.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import JoinGroupForm from "@/app/(login)/join-group/_components/joinGroupForm/joinGroupForm";
 import CreateGroupForm from "@/components/ui/createGroupForm/createGroupForm";
 import { Button, Modal } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
@@ -13,6 +14,7 @@ export default function CreateGroupModal(){
     <>
       <Modal opened={opened} onClose={close} closeOnClickOutside={!isPending} closeOnEscape={!isPending} withCloseButton={!isPending} size={"lg"}>
         <CreateGroupForm onPendingChange={setIsPending}/>
+        <JoinGroupForm />
       </Modal>
       <Button onClick={open} >グループの作成・参加</Button>
     </>
