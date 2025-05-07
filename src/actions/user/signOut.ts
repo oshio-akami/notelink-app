6188cb430ea3b508cea1e5ce40e7933acdc788b3
shion_inoue <shion.akasal@gmail.com>
2025-05-07 14:18:49 +0900

    Docコメント追加

--- src/actions/user/signOut.ts
+++ src/actions/user/signOut.ts
@@ -6,17 +6,18 @@ import { accounts, users } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { signOut as authSignOut} from "@/auth";
 
+/**
+ * ユーザーのサインアウト処理を行う関数。
+ * 認証情報を確認し、ゲストユーザーの場合アカウントを削除する。
+ */
 export default async function signOut(){
-  console.log("signout:");
   const session=await auth();
   if(!session?.user?.id){
     return;
   }
-  console.log(session?.user?.id);
   const account=await db.select().from(accounts).where(eq(accounts.userId,session.user.id)).limit(1);
   if(account[0]&&account[0].provider==="credentials"){
     await db.delete(users).where(eq(users.id,session.user.id));
   }
-
   await authSignOut();
 }
