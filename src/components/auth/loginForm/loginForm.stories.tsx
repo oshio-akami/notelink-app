d0557352e2b90249698b15e0f2b3b0c97972f5ac
shion_inoue <shion.akasal@gmail.com>
2025-05-14 14:06:26 +0900

    認証関係のui修正とstories追加

---
+++ src/components/auth/loginForm/loginForm.stories.tsx
@@ -0,0 +1,34 @@
+import { createMock } from "storybook-addon-module-mock";
+import * as createPreviewGroupModule from "@/actions/group/createPreviewGroup";
+import { LoginForm } from "./loginForm";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const mockModule = createMock(createPreviewGroupModule, "default");
+
+const mockCreatePreviewGroup = mockModule.mockImplementation(async () => {
+  console.log("Mock createPreviewGroup");
+});
+
+const meta: Meta<typeof LoginForm> = {
+  title: "Auth/LoginForm",
+  component: LoginForm,
+  tags: ["autodocs"],
+  parameters: {
+    nextjs: {
+      navigation: {
+        searchParams: { callbackUrl: "/mocked-url" },
+      },
+    },
+    moduleMock: {
+      mocks: {
+        "@/actions/group/createPreviewGroup": {
+          default: mockCreatePreviewGroup,
+        },
+      },
+    },
+  },
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof LoginForm> = {};
