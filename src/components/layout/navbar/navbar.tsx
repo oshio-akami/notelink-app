b36b0b45db36e4f05a694e82a8399ea437673795
shion_inoue <shion.akasal@gmail.com>
2025-05-20 12:02:38 +0900

    Navbarの修正

--- src/components/layout/navbar/navbar.tsx
+++ src/components/layout/navbar/navbar.tsx
@@ -1,34 +1,29 @@
-"use client";
-
 import styles from "./navbar.module.scss";
 import {
   IconDashboard,
   IconUsersGroup,
   IconClipboardText,
 } from "@tabler/icons-react";
-import { NavLink } from "@mantine/core";
-import { redirect } from "next/navigation";
-import { usePathname } from "next/navigation";
-import { Text } from "@mantine/core";
+import HomeMenu from "@/components/navbar/homeMenu/homeMenu";
+import GroupMenu from "@/components/navbar/groupMenu/groupMenu";
+import { getClient } from "@/libs/hono";
 
 const iconSize = 24;
-const linksMockData = [
+
+const homeMenuData = [
   {
     label: "ホーム",
     link: "/home",
-    active: "home",
     icon: <IconDashboard size={iconSize} stroke={1.5} />,
   },
   {
     label: "投稿",
     link: "/post",
-    active: "post",
     icon: <IconClipboardText size={iconSize} stroke={1.5} />,
   },
   {
     label: "メンバー",
     link: "/member",
-    active: "member",
     icon: <IconUsersGroup size={iconSize} stroke={1.5} />,
   },
   //{label:'設定',link:'/settings/userSetting',active:"settings",icon:<IconSettings size={iconSize} stroke={1.5} />},
@@ -38,32 +33,23 @@ type Props = {
   id: string;
 };
 
-const createLink = (id: string, link: string) => {
-  return `/group/${id}${link}`;
+/**API からユーザーの現在のグループの情報を取得する非同期関数 */
+const getGroups = async () => {
+  const client = await getClient();
+  const res = await client.api.user.groups.$get();
+  const body = await res.json();
+  return body.groups;
 };
 
-export function NavBar({ id }: Props) {
-  const path = usePathname();
-  const links = linksMockData.map((target) => (
-    <NavLink
-      className={styles.nav}
-      key={target.link}
-      onClick={() => redirect(createLink(id, target.link))}
-      label={target.label}
-      leftSection={target.icon}
-      active={path.indexOf(createLink(id, target.link)) !== -1}
-      color="#f0f0f0"
-      variant="filled"
-      autoContrast
-    />
-  ));
+/**ナビゲーションバーコンポーネント */
+export async function NavBar({ id }: Props) {
+  const groups = await getGroups();
   return (
     <>
       <nav className={styles.navbar}>
-        <Text className={styles.label}>グループメニュー</Text>
-        {links}
+        <HomeMenu menus={homeMenuData} groupId={id} />
+        {groups && <GroupMenu groups={groups} />}
       </nav>
-      ;
     </>
   );
 }
