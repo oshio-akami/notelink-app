53059aa074551d429ad190974d990b72d51256dc
shion_inoue <shion.akasal@gmail.com>
2025-05-30 10:39:20 +0900

    bookmark用のapi作成

--- src/app/api/[...route]/article.ts
+++ src/app/api/[...route]/article.ts
@@ -3,10 +3,10 @@ import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 import { handleApiError } from "@/libs/handleApiError";
 import { db } from "@/db";
-import { articles, userProfiles } from "@/db/schema";
-import { hasJoinedGroup } from "@/libs/apiUtils";
+import { articles, bookmarks, userProfiles } from "@/db/schema";
+import { hasJoinedGroup, withGroupMemberCheck } from "@/libs/apiUtils";
 import { eq, desc, and } from "drizzle-orm";
-import { auth } from "@/auth";
+import { getSessionUserId } from "@/libs/getSessionUserId";
 
 export const runtime = "edge";
 
@@ -31,13 +31,13 @@ const article = new Hono()
         if (!hasJoined) {
           return c.json({ articles: null }, 404);
         }
-        const session = await auth();
-        if (!session?.user?.id) {
+        const userId = await getSessionUserId();
+        if (!userId) {
           return c.json({ articles: null }, 401);
         }
         const whereConditions = [eq(articles.groupId, groupId)];
         if (mine) {
-          whereConditions.push(eq(articles.userId, session?.user?.id));
+          whereConditions.push(eq(articles.userId, userId));
         }
         const articleList = await db
           .select({
@@ -46,18 +46,95 @@ const article = new Hono()
               displayName: userProfiles.displayName,
               image: userProfiles.image,
             },
+            id: articles.id,
             title: articles.title,
             content: articles.content,
             image: articles.image,
             createdAt: articles.createdAt,
+            isBookmark: bookmarks.articleId,
           })
           .from(articles)
           .innerJoin(userProfiles, eq(articles.userId, userProfiles.userId))
+          .leftJoin(
+            bookmarks,
+            and(
+              eq(articles.id, bookmarks.articleId),
+              eq(bookmarks.userId, userId)
+            )
+          )
           .where(and(...whereConditions))
           .orderBy(desc(articles.createdAt));
-        return c.json({ articles: articleList }, 200);
+        const replaceArticleList = articleList.map((article) => ({
+          ...article,
+          isBookmark: article.isBookmark != null,
+        }));
+        return c.json({ articles: replaceArticleList }, 200);
       } catch (error) {
         return handleApiError(c, error, { articles: false });
+      }
+    }
+  )
+  .post(
+    "/bookmark/:articleId",
+    zValidator(
+      "param",
+      z.object({
+        articleId: z.string().uuid(),
+      })
+    ),
+    async (c) => {
+      try {
+        const { articleId } = await c.req.valid("param");
+        const userId = await getSessionUserId();
+        if (!userId) {
+          return c.json({ success: false }, 403);
+        }
+        const bookmark = await db
+          .insert(bookmarks)
+          .values({
+            userId: userId,
+            articleId: articleId,
+          })
+          .returning();
+        if (bookmark.length === 0) {
+          return c.json({ success: false });
+        }
+        return c.json({ success: true }, 200);
+      } catch (error) {
+        return handleApiError(c, error, { success: false });
+      }
+    }
+  )
+  .delete(
+    "/bookmark/:articleId",
+    zValidator(
+      "param",
+      z.object({
+        articleId: z.string().uuid(),
+      })
+    ),
+    async (c) => {
+      try {
+        const { articleId } = await c.req.valid("param");
+        const userId = await getSessionUserId();
+        if (!userId) {
+          return c.json({ success: false }, 403);
+        }
+        const deleted = await db
+          .delete(bookmarks)
+          .where(
+            and(
+              eq(bookmarks.userId, userId),
+              eq(bookmarks.articleId, articleId)
+            )
+          )
+          .execute();
+        if (deleted.rowCount === 0) {
+          return c.json({ success: false }, 404);
+        }
+        return c.json({ success: true }, 200);
+      } catch (error) {
+        return handleApiError(c, error, { success: false });
       }
     }
   )
@@ -72,30 +149,21 @@ const article = new Hono()
     async (c) => {
       try {
         const { groupId } = await c.req.valid("param");
-        const hasJoined = await hasJoinedGroup(groupId);
-        if (!hasJoined) {
-          return c.json({ articles: null }, 404);
+        const check = await withGroupMemberCheck(groupId);
+        if (!check.success) {
+          return c.json({ bookmarkList: null }, check.status);
         }
-        const articleList = await db
+        const bookmarkList = await db
           .select({
-            userProfiles: {
-              userId: userProfiles.userId,
-              displayName: userProfiles.displayName,
-              image: userProfiles.image,
-            },
-            id: articles.id,
-            title: articles.title,
-            content: articles.content,
-            image: articles.image,
-            createdAt: articles.createdAt,
+            userId: bookmarks.userId,
+            articleId: bookmarks.articleId,
           })
-          .from(articles)
-          .innerJoin(userProfiles, eq(articles.userId, userProfiles.userId))
-          .where(eq(articles.groupId, groupId))
-          .orderBy(desc(articles.createdAt));
-        return c.json({ articles: articleList }, 200);
+          .from(bookmarks)
+          .where(and(eq(bookmarks.userId, check.userId)))
+          .orderBy(desc(bookmarks.createdAt));
+        return c.json({ bookmarkList: bookmarkList }, 200);
       } catch (error) {
-        return handleApiError(c, error, { articles: false });
+        return handleApiError(c, error, { bookmarkList: null });
       }
     }
   )
@@ -112,14 +180,14 @@ const article = new Hono()
     ),
     async (c) => {
       try {
-        const session = await auth();
-        if (!session?.user?.id) {
-          return c.json({ created: null }, 401);
-        }
         const { groupId, title, image, content } = c.req.valid("json");
+        const check = await withGroupMemberCheck(groupId);
+        if (!check.success) {
+          return c.json({ created: null }, check.status);
+        }
         const created = await db.insert(articles).values({
           groupId: groupId,
-          userId: session.user.id,
+          userId: check.userId,
           title: title,
           content: content,
           image: image,
@@ -140,10 +208,6 @@ const article = new Hono()
     ),
     async (c) => {
       try {
-        const session = await auth();
-        if (!session?.user?.id) {
-          return c.json({ deleted: null }, 401);
-        }
         const { articleId } = c.req.valid("param");
         const deleted = await db
           .delete(articles)
