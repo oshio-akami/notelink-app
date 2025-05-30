a164a2b588cddd43782d5b93432132bff0729044
shion_inoue <shion.akasal@gmail.com>
2025-05-30 10:34:31 +0900

    アイコンボタンコンポーネントに状態の切り替えを追加

--- src/components/shared/iconButton/iconButton.tsx
+++ src/components/shared/iconButton/iconButton.tsx
@@ -1,10 +1,30 @@
 import styles from "./iconButton.module.scss";
 import type { IconProps } from "@tabler/icons-react";
+import { useState } from "react";
 
 type Props = {
   icon: React.ReactElement<IconProps>;
+  activeIcon?: React.ReactElement<IconProps>;
+  onClick?: (isActive: boolean) => void;
+  defaultActive?: boolean;
 };
 
-export default function IconButton({ icon }: Props) {
-  return <div className={styles.button}>{icon}</div>;
+export default function IconButton({
+  icon,
+  activeIcon = icon,
+  onClick = () => {},
+  defaultActive = false,
+}: Props) {
+  const [isActive, setActive] = useState(defaultActive);
+  return (
+    <div
+      className={styles.button}
+      onClick={() => {
+        setActive(!isActive);
+        onClick(!isActive);
+      }}
+    >
+      {isActive && activeIcon ? activeIcon : icon}
+    </div>
+  );
 }
