b703ab7bb301cf1da48b43177b5fbc2dec9f2764
oshio-akam <oshio.akaml@gmail.com>
2025-06-05 11:45:50 +0900

    コメント関係のサーバーアクション追加

--- src/actions/article/articleActions.ts
+++ src/actions/article/articleActions.ts
@@ -1,7 +1,8 @@
 "use server";
 
+import { withGroupMemberCheck } from "@/libs/apiUtils";
 import { getClient } from "@/libs/hono";
-import { PostFormSchema } from "@/utils/types/formSchema";
+import { postCommentSchema, PostFormSchema } from "@/utils/types/formSchema";
 import { parseWithZod } from "@conform-to/zod";
 
 /**
@@ -10,7 +11,7 @@ import { parseWithZod } from "@conform-to/zod";
  * @param {FormData} formData - 投稿フォームのデータ
  * @returns {Promise<{status: string, message: string}>} - 操作の結果（成功または失敗のメッセージ）
  */
-export async function postArticle(_: unknown, formData: FormData) {
+export const postArticle = async (_: unknown, formData: FormData) => {
   const submission = parseWithZod(formData, { schema: PostFormSchema });
   if (submission.status !== "success") {
     return submission.reply();
@@ -30,7 +31,51 @@ export async function postArticle(_: unknown, formData: FormData) {
     return { status: "error", message: "投稿に失敗しました" };
   }
   return { status: "success", message: "投稿しました" };
-}
+};
+
+/**コメントを投稿する非同期関数 */
+export const postComment = async (_: unknown, fromData: FormData) => {
+  const submission = parseWithZod(fromData, { schema: postCommentSchema });
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+  const { groupId, articleId, comment } = submission.value;
+  const client = await getClient();
+  const res = await client.api.article.comment[":groupId"][":articleId"].$post({
+    param: {
+      groupId: groupId,
+      articleId: articleId,
+    },
+    json: { comment: comment },
+  });
+  const body = await res.json();
+  if (body.success) {
+    return { status: "success", message: "コメントを送信しました" };
+  } else {
+    return { status: "error", message: "コメントを送信に失敗しました" };
+  }
+};
+
+/**コメントを削除する非同期関数 */
+export const deleteComment = async (commentId: string, groupId: string) => {
+  const client = await getClient();
+  const res = await client.api.article.comment[":groupId"][
+    ":commentId"
+  ].$delete({
+    param: {
+      groupId: groupId,
+      commentId: commentId,
+    },
+  });
+  if (!res.ok) {
+    throw new Error("コメントの削除に失敗しました");
+  }
+  const body = await res.json();
+  if (!body.success) {
+    throw new Error("サーバーから失敗レスポンスが返されました");
+  }
+  return { success: true };
+};
 
 type UploadResponse = {
   success: boolean;
