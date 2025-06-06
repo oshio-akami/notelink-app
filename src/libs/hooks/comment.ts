0092d1a4eb5962873d678e06f3027f4f4a662b5c
oshio-akam <oshio.akaml@gmail.com>
2025-06-06 13:52:11 +0900

    コメントのカスタムフックに投稿、削除用のイベント追加

--- src/libs/hooks/comment.ts
+++ src/libs/hooks/comment.ts
@@ -1,7 +1,13 @@
+"use client";
+
 import useSWR from "swr";
 import client from "@/libs/honoClient";
+import { deleteComment, postComment } from "@/actions/article/articleActions";
+import { formatDate } from "../utils";
+import { Comment } from "@/utils/types/articleType";
 
-export const useArticleComments = (groupId: string, articleId: string) => {
+/**コメントのカスタムフック */
+export const useArticleComment = (groupId: string, articleId: string) => {
   const fetcher = async () => {
     const res = await client.api.article.comments[":groupId"][
       ":articleId"
@@ -13,12 +19,62 @@ export const useArticleComments = (groupId: string, articleId: string) => {
   };
   const key = `/comments/${groupId}/${articleId}`;
 
-  const { data, error, isLoading, mutate } = useSWR(key, fetcher);
+  const { data, error, isLoading, mutate } = useSWR<Comment[]>(key, fetcher);
+
+  /**コメントを削除する関数 */
+  const handleDeleteComment = async (commentId: string) => {
+    mutate(
+      (prev) => prev?.filter((comment) => comment.id !== commentId),
+      false
+    );
+    try {
+      await deleteComment(commentId, groupId);
+    } catch {
+      mutate();
+    }
+  };
+
+  /**コメントを投稿する関数 */
+  const handlePostComment = async (
+    userProfile: { userId: string; displayName: string; image: string },
+    comment: string
+  ) => {
+    const tempId = `temp-${Date.now()}`;
+    const optimisticComment = {
+      id: tempId,
+      content: comment,
+      articleId: articleId,
+      groupId: groupId,
+      userId: userProfile.userId,
+      createdAt: new Date().toString(),
+      userProfiles: {
+        userId: userProfile.userId,
+        displayName: userProfile?.displayName ?? "",
+        image: userProfile?.image ?? "",
+      },
+    } as Comment;
+
+    try {
+      mutate((prev) => [optimisticComment, ...(prev ?? [])], false);
+
+      await postComment(comment, groupId, articleId);
+      mutate();
+    } catch {
+      mutate((prev = []) => prev.filter((c) => c.id !== tempId), false);
+    }
+  };
 
   return {
+    /** コメント一覧*/
     comments: data,
+    /**コメント一覧の取得が完了しているかを表すフラグ */
     isLoading,
+    /**コメント一覧取得時のエラー */
     error,
     mutate,
+    /**コメントを削除する関数 */
+    handleDeleteComment,
+    /**コメントを投稿する関数 */
+    handlePostComment,
   };
 };
