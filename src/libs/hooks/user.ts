ea02e6060bb10f9275eec2e6ace2643060962b1c
oshio-akam <oshio.akaml@gmail.com>
2025-06-09 14:04:17 +0900

    userProfileのhookに管理者かどうかのフラグ追加

--- src/libs/hooks/user.ts
+++ src/libs/hooks/user.ts
@@ -2,16 +2,22 @@
 
 import useSWR from "swr";
 import client from "@/libs/honoClient";
+import { isRoleAdmin, ROLE_MEMBER } from "../roleUtils";
 
-export const useProfile = () => {
+export const useProfile = (groupId: string) => {
   const fetcher = async () => {
-    const res = await client.api.user.profile.$get();
+    const res = await client.api.user[":groupId"].profile.$get({
+      param: {
+        groupId: groupId,
+      },
+    });
     const body = await res.json();
     return body.profile;
   };
   const { data, error, isLoading, mutate } = useSWR("/profile", fetcher);
   return {
     profile: data,
+    isRoleAdmin: isRoleAdmin(data?.roleId ?? ROLE_MEMBER),
     isLoading,
     error,
     mutate,
