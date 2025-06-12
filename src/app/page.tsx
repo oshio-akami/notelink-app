ee35c6dc3d21fff88cd4197c5c4ba2693fa820d4
oshio-akam <oshio.akaml@gmail.com>
2025-06-12 22:36:55 +0900

    ルートページのsrcのurl差し替えとレスポンシブ調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,18 @@
 import Link from "next/link";
 import { Text, Button, Image } from "@mantine/core";
 import styles from "./page.module.scss";
-import { useMediaQuery } from "@mantine/hooks";
 
 export default function Page() {
-  const matches = useMediaQuery("(min-width: 1200px)");
   return (
     <div className={styles.pageWrapper}>
       <div className={styles.header}>
-        <Image
-          w={200}
-          src="https://pub-0e85cec67fe344ccb5094d3659571d7d.r2.dev/sample_logo.png"
-          alt=""
-        />
+        <Image w={200} src="sample_logo.png" alt="" />
       </div>
       <div className={styles.contentWrapper}>
         <div className={styles.leftSection}>
           <div className={styles.catchCopy}>
-            <Text c="#191970" fw={700} size={matches ? "4rem" : "3rem"}>
-              仲間とつながる
-            </Text>
-            <Text c="#191970" fw={700} size={matches ? "4rem" : "3rem"}>
-              情報共有スペース
-            </Text>
+            <Text>仲間とつながる</Text>
+            <Text>情報共有スペース</Text>
           </div>
           <div className={styles.subtitle}>
             <Text>招待された仲間だけが集うグループ空間。</Text>
@@ -36,14 +26,9 @@ export default function Page() {
             <Button size="lg">今すぐ始める</Button>
           </Link>
         </div>
-        {matches && (
-          <div className={styles.rightSection}>
-            <Image
-              src="https://pub-0e85cec67fe344ccb5094d3659571d7d.r2.dev/sample_image.png"
-              alt=""
-            />
-          </div>
-        )}
+        <div className={styles.rightSection}>
+          <Image src="sample_image.png" alt="" />
+        </div>
       </div>
     </div>
   );
