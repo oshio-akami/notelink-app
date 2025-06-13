8e2a695882b4ba6e1d4528c01802899f61192983
oshio-akam <oshio.akaml@gmail.com>
2025-06-13 14:14:36 +0900

    headerのレスポンシブ対応

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,16 +1,26 @@
 "use client";
 
 import styles from "./header.module.scss";
-import { IconBell } from "@tabler/icons-react";
 import { ProfileWindow } from "@/components/auth/profileWindow/profileWindow";
-import { Burger, Button, Image, Text } from "@mantine/core";
+import { Burger, Button, Image, Text, useMantineTheme } from "@mantine/core";
 import GroupAccessModal from "@/components/group/groupAccessModal/groupAccessModal";
-import { useDisclosure } from "@mantine/hooks";
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 import { useProfile } from "@/libs/hooks/user";
 import { useGroup } from "@/libs/context/groupContext/groupContext";
 import IconButton from "@/components/shared/iconButton/iconButton";
+import { IconUsersGroup } from "@tabler/icons-react";
 
 export const runtime = "edge";
+
+function sliceText(text: string, maxLength: number) {
+  if (text.length <= maxLength) {
+    return text;
+  }
+  if (maxLength <= 3) {
+    return text.slice(0, maxLength);
+  }
+  return text.slice(0, maxLength - 3) + "...";
+}
 
 type Props = {
   burgerOpened: boolean;
@@ -21,6 +31,8 @@ export function Header({ burgerOpened, onClickBurger }: Props) {
   const { groupId, groupName } = useGroup();
   const { profile } = useProfile(groupId);
   const [opened, { open, close }] = useDisclosure(false);
+  const theme = useMantineTheme();
+  const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
   return (
     <div className={styles.header}>
       <div className={styles.leftSection}>
@@ -36,8 +48,7 @@ export function Header({ burgerOpened, onClickBurger }: Props) {
           src="https://pub-0e85cec67fe344ccb5094d3659571d7d.r2.dev/sample_logo.png"
           alt="logo"
         />
-        <Text>|</Text>
-        <Text>{groupName}</Text>
+        <Text>{sliceText(groupName, 10)}</Text>
       </div>
       <div className={styles.rightSection}>
         {profile && profile !== undefined && (
@@ -53,8 +64,12 @@ export function Header({ burgerOpened, onClickBurger }: Props) {
             )}
           </ProfileWindow>
         )}
-        <IconButton icon={<IconBell />} />
-        <Button onClick={open}>グループの作成・参加</Button>
+        {isMobile ? (
+          <IconButton icon={<IconUsersGroup />} onClick={open} border />
+        ) : (
+          <Button onClick={open}>グループの作成・参加</Button>
+        )}
+
         <GroupAccessModal opened={opened} onClose={close} />
       </div>
     </div>
