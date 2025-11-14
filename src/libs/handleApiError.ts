6fab13a4193ac88754366be23f726a55ddce2efb
oshio-akam <oshio.akaml@gmail.com>
2025-11-14 14:32:53 +0900

    handleApiErrorを修正

--- src/libs/handleApiError.ts
+++ src/libs/handleApiError.ts
@@ -1,11 +1,23 @@
+import {} from "@/services/group";
+import { ForbiddenError, NotFoundError } from "@/utils/errors";
 import { Context } from "hono";
 import { ZodError } from "zod";
 
-type Response=Record<string,unknown>
+type Response = Record<string, unknown>;
 
-export function handleApiError(c:Context,error:unknown,fallback:Response={}){
-  if(error instanceof ZodError){
-    return c.json({...fallback},422)
+export function handleApiError(
+  c: Context,
+  error: unknown,
+  fallback: Response = {}
+) {
+  if (error instanceof ZodError) {
+    return c.json({ ...fallback }, 422);
   }
-  return c.json({...fallback},500)
+  if (error instanceof ForbiddenError) {
+    return c.json({ ...fallback }, 403);
+  }
+  if (error instanceof NotFoundError) {
+    return c.json({ ...fallback }, 404);
+  }
+  return c.json({ ...fallback }, 500);
 }
