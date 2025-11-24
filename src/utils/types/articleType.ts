49b947d128de10df126ed8bd252ad3991a710084
oshio-akam <oshio.akaml@gmail.com>
2025-11-24 10:31:01 +0900

    記事関係の型定義追加

--- src/utils/types/articleType.ts
+++ src/utils/types/articleType.ts
@@ -1,3 +1,4 @@
+/**APIから取得する記事 */
 export type Article = {
   userProfiles: {
     userId: string;
@@ -13,6 +14,39 @@ export type Article = {
   commentCount: number;
 };
 
+/**DBから取得後正規化した記事 */
+export type DBNormalizedArticle = {
+  userProfiles: {
+    userId: string;
+    displayName: string;
+    image: string;
+  };
+  id: string;
+  title: string;
+  content: string;
+  image: string;
+  createdAt: Date;
+  isBookmark: boolean;
+  commentCount: number;
+};
+
+/**DBから取得する記事 */
+export type DBArticle = {
+  userProfiles: {
+    userId: string;
+    displayName: string;
+    image: string;
+  };
+  id: string;
+  title: string;
+  content: string;
+  image: string;
+  createdAt: Date;
+  isBookmark: string | null;
+  commentCount: number;
+};
+
+/**APIから取得するコメント */
 export type Comment = {
   userProfiles: {
     userId: string;
@@ -25,4 +59,32 @@ export type Comment = {
   userId: string;
   createdAt: string;
   content: string;
+};
+
+/**APIから取得するブックマーク */
+export type Bookmark = {
+  userId: string;
+  createdAt: Date;
+  articleId: string;
+};
+
+/**Postするときの記事の構造 */
+export type PostArticle = {
+  groupId: string;
+  id: string;
+  image: string;
+  userId: string;
+  createdAt: Date;
+  title: string;
+  content: string;
+};
+
+/**Postするときのコメントの構造 */
+export type PostComment = {
+  groupId: string;
+  articleId: string;
+  id: string;
+  userId: string;
+  createdAt: Date;
+  comment: string;
 };
